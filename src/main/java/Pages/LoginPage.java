33cea6523180becf5f2fded30cd6480cd2d90d44
Juan Bryan Estrada Acosta <bestrada@belatrix.com>
2016-08-22 09:40:28 -0500

    first commit

---
+++ src/main/java/Pages/LoginPage.java
@@ -0,0 +1,18 @@
+package Pages;
+
+import java.net.URL;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class LoginPage {
+	
+	public static void GetComponent()
+	{
+		
+	}
+
+}
